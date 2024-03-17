09a0dfa1d9ac297f1d7536bad3719a36229ef55a
Marcos Antonio <62305370+antoniomrrds@users.noreply.github.com>
2024-03-17 20:59:30 -0300

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+Import-Module -Name Terminal-Icons
+Import-Module Az.Tools.Predictor
+
+oh-my-posh init pwsh --config 'C:\Users\anton\AppData\Local\Programs\oh-my-posh\themes\unicorn.omp.json' | Invoke-Expression
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
